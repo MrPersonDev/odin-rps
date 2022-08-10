b29752f6e06f60c41da7a903541795732e0ec168
MrPersonDev <simonashton37@gmail.com>
2022-08-10 10:40:47 -0700

    Get player input

--- script.js
+++ script.js
@@ -6,3 +6,12 @@ function getComputerChoice() {
     return Math.floor(Math.random() * 3);
 }
 
+function getPlayerChoice() {
+    let choice
+    do {
+        choice = prompt("Rock Paper or Scissors") 
+    }
+    while (!choices.includes(choice))
+
+    return choices.indexOf(choice)
+}
