953ede0eb52b42851d45439de06ae0f468888b48
MrPersonDev <simonashton37@gmail.com>
2022-08-10 10:41:10 -0700

    Get winner

--- script.js
+++ script.js
@@ -15,3 +15,15 @@ function getPlayerChoice() {
 
     return choices.indexOf(choice)
 }
+
+function getWinner(player1, player2) {
+    if (player1 == player2) {
+        return 0
+    }
+    else if (player1 > player2 || player2 == choices.length() && player1 == 0) {
+        return 1
+    }
+    else {
+        return 2
+    }
+}
